4aa4c5d1bc933080c18842c459e6979b1bc39c09
cosmosapjw_quantum <cosmosapjw@gmail.com>
2022-12-06 15:29:01 +0900

    added mcmc files

---
+++ mcmc/class_pl_bao_sn.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+source /appl/intel/oneapi/setvars.sh
+source /appl/intel/oneapi/mpi/2021.2.0/env/vars.sh
+source /appl/intel/oneapi/compiler/latest/env/vars.sh intel64
+source /appl/intel/oneapi/mpi/latest/env/vars.sh intel64
+export I_MPI_F90=ifort
+
+##test mpirun
+mpirun -n 2 python -c "from mpi4py import MPI, __version__; print(__version__ if MPI.COMM_WORLD.Get_rank() else '')"
+
+##real code
+mpirun -n 4 --map-by socket:PE=16 cobaya-run setting_class_pl_bao_sn.yaml -p /home/jwpark/comodata -o chains/class_pl_bao_sn -r
+
+wait
+
